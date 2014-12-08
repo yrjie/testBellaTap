c0728cb0f3871ed772e1a5957cce849c80261b37
yrjie <yrjie0@gmail.com>
2014-12-08 22:12:25 +0800

    connected to belladati and got the service

--- src/main/java/com/ruijie/testBellaTap/pages/BelladatiDemo.java
+++ src/main/java/com/ruijie/testBellaTap/pages/BelladatiDemo.java
@@ -1,9 +1,14 @@
 package com.ruijie.testBellaTap.pages;
 
+import java.net.URL;
+
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.tapestry5.annotations.*;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Request;
+import org.apache.tapestry5.services.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.belladati.sdk.BellaDati;
@@ -17,14 +22,33 @@ public class BelladatiDemo {
 	private static final String SESSION_OAUTH_ATTRIBUTE = "pendingOAuth";
 	private final BellaDatiConnection connection = BellaDati.connect();
 	
-	@Autowired
-	private HttpSession session;
-	
 	@Property
 	private DataSet dataset;
 	
 	@Property
 	private int a;
+	
+	@Property
+	//@SessionAttribute(SESSION_OAUTH_ATTRIBUTE)
+	private OAuthRequest request;
+	
+	//@SessionAttribute(SESSION_SERVICE_ATTRIBUTE)
+	//BellaDatiService service;
+	
+	@SessionAttribute("testStr")
+	String str1="";
+	
+	@Inject
+	private Request request1;
+	
+	@Persist
+	private Session session;
+	
+	@Property
+	private BellaDatiService s2;
+	
+	@Property
+	private String s1;
 	
 	private String getDeploymentUrl(HttpServletRequest request) {
 		String requestUrl = request.getRequestURL().toString();
@@ -34,20 +58,38 @@ public class BelladatiDemo {
 	
 	public OAuthRequest initiateOAuth(String redirectUrl) {
 		//OAuthRequest request = connection.oAuth("techKey", "techSecret", redirectUrl);
-		OAuthRequest request = connection.oAuth("techKey", "techSecret");
+		request = connection.oAuth("techKey", "techSecret", "http://localhost:8080/testBellaTap/belladatidemo");
+		//service = request.requestAccess();
+		//session.setAttribute(SESSION_OAUTH_ATTRIBUTE, request);
+		//s1=(BellaDatiService)request1.getSession(false).getAttribute(SESSION_SERVICE_ATTRIBUTE);
+		str1="311aa";
+		//s1=(String)session.getAttribute("testStr");
+		s1=session.toString();
 		session.setAttribute(SESSION_OAUTH_ATTRIBUTE, request);
 		return request;
 	}
 	
-	public BellaDatiService getService() {
-		return (BellaDatiService) session.getAttribute(SESSION_SERVICE_ATTRIBUTE);
+	URL onActionFromLogin(){
+		//return new URL("https://service.belladati.com/authorizeRequestToken/d38a2deeaecc85d75e37cc47893a08d8/techKey");
+		OAuthRequest req=initiateOAuth(null);
+		return req.getAuthorizationUrl();
+	}
+	
+	public boolean getLoggedIn(){
+		return session.getAttribute(SESSION_SERVICE_ATTRIBUTE)!=null;
+		//return dataset!=null;
+	}
+	
+	void onActionFromShowDataset(){
+		String datasetId="18812";
+		BellaDatiService service = ((OAuthRequest)session.getAttribute(SESSION_OAUTH_ATTRIBUTE)).requestAccess();
+		session.setAttribute(SESSION_SERVICE_ATTRIBUTE, service);
+		dataset=((BellaDatiService)session.getAttribute(SESSION_SERVICE_ATTRIBUTE)).loadDataSet(datasetId);
 	}
 	
 	@BeginRender
 	void doBeginRender() {
-		String datasetId="3231";
 		a=10;
-		initiateOAuth(null);
-		//dataset=getService().loadDataSet(datasetId);
+		session=request1.getSession(true);
 	}
 }
