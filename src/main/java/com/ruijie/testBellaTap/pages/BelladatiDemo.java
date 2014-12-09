b283c6a3fcb0676339b6cf997561dcf2c3e72c7f
yrjie <yrjie@qq.com>
2014-12-09 17:00:11 +0800

    accessed all the dataset, dashboard and reports

--- src/main/java/com/ruijie/testBellaTap/pages/BelladatiDemo.java
+++ src/main/java/com/ruijie/testBellaTap/pages/BelladatiDemo.java
@@ -1,6 +1,10 @@
 package com.ruijie.testBellaTap.pages;
 
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +19,11 @@ import com.belladati.sdk.BellaDati;
 import com.belladati.sdk.BellaDatiConnection;
 import com.belladati.sdk.BellaDatiService;
 import com.belladati.sdk.auth.OAuthRequest;
+import com.belladati.sdk.dashboard.DashboardInfo;
 import com.belladati.sdk.dataset.DataSet;
+import com.belladati.sdk.dataset.DataSetInfo;
+import com.belladati.sdk.report.ReportInfo;
+import com.belladati.sdk.util.PaginatedIdList;
 
 public class BelladatiDemo {
 	private static final String SESSION_SERVICE_ATTRIBUTE = "BellaDatiService";
@@ -53,6 +61,15 @@ public class BelladatiDemo {
 	@Persist
 	private String s1;
 	
+	@Property
+	private DashboardInfo _dbi;
+	
+	@Property
+	private ReportInfo _rpi;
+	
+	@Property
+	private DataSetInfo _dsi;
+	
 	private String getDeploymentUrl(HttpServletRequest request) {
 		String requestUrl = request.getRequestURL().toString();
 		String servletPath = request.getServletPath();
@@ -61,13 +78,13 @@ public class BelladatiDemo {
 	
 	public OAuthRequest initiateOAuth(String redirectUrl) {
 		//OAuthRequest request = connection.oAuth("techKey", "techSecret", redirectUrl);
-		request = connection.oAuth("techKey", "techSecret", "http://localhost:8080/testBellaTap/belladatidemo");
+		request = connection.oAuth("techKey", "techSecret", "http://localhost:8080/testBellaTap/belladatidemo.authorize");
 		//service = request.requestAccess();
 		//session.setAttribute(SESSION_OAUTH_ATTRIBUTE, request);
 		//s1=(BellaDatiService)request1.getSession(false).getAttribute(SESSION_SERVICE_ATTRIBUTE);
 		str1="311aa";
 		//s1=(String)session.getAttribute("testStr");
-		s1=session.toString();
+		s1=request1.getPath();
 		return request;
 	}
 	
@@ -77,6 +94,10 @@ public class BelladatiDemo {
 		return req.getAuthorizationUrl();
 	}
 	
+	public BellaDatiService getService() {
+		return (BellaDatiService) session.getAttribute(SESSION_SERVICE_ATTRIBUTE);
+	}
+	
 	public boolean getLoggedIn(){
 		return session.getAttribute(SESSION_SERVICE_ATTRIBUTE)!=null;
 		//return dataset!=null;
@@ -84,9 +105,43 @@ public class BelladatiDemo {
 	
 	void onActionFromShowDataset(){
 		String datasetId="18812";
+//		BellaDatiService service = ((OAuthRequest)session.getAttribute(SESSION_OAUTH_ATTRIBUTE)).requestAccess();
+//		session.setAttribute(SESSION_SERVICE_ATTRIBUTE, service);
+		dataset=((BellaDatiService)session.getAttribute(SESSION_SERVICE_ATTRIBUTE)).loadDataSet(datasetId);
+	}
+	
+	URL onActionFromAuthorize(){
 		BellaDatiService service = ((OAuthRequest)session.getAttribute(SESSION_OAUTH_ATTRIBUTE)).requestAccess();
 		session.setAttribute(SESSION_SERVICE_ATTRIBUTE, service);
-		dataset=((BellaDatiService)session.getAttribute(SESSION_SERVICE_ATTRIBUTE)).loadDataSet(datasetId);
+		try {
+			URL redir=new URL("http://localhost:8080/testBellaTap/belladatidemo");
+			return redir;
+		}
+		catch (MalformedURLException ex){
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+	public List<DashboardInfo> getDashboardInfo(){
+		BellaDatiService service=getService();
+		if (service!=null)
+			return service.getDashboardInfo().load().toList();
+		return null;
+	}
+	
+	public List<ReportInfo> getReportInfo(){
+		BellaDatiService service=getService();
+		if (service!=null)
+			return service.getReportInfo().load().toList();
+		return null;
+	}
+	
+	public List<DataSetInfo> getDataSetInfo(){
+		BellaDatiService service=getService();
+		if (service!=null)
+			return service.getDataSetInfo().load().toList();
+		return null;
 	}
 	
 	@BeginRender
